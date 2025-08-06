a7c655d3491727b00e3114942edf385a2e8ee047
LawlietDN <ufcgamer82@gmail.com>
2025-08-06 20:20:06 +0300

    Implemented node entry point.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <AsioPDS/node.h>
+int main()
+{
+    try
+    {
+        boost::asio::io_context io;
+        Node node(io);
+        io.run();            
+    }
+    catch(std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what();
+    }
+}
