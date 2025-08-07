a9fef9eafa4042e78bfb044ae95561f527e88ce5
LawlietDN <ufcgamer82@gmail.com>
2025-08-07 22:17:51 +0300

    node.h: Extended node interface for peer management

--- Include/AsioPDS/node.h
+++ Include/AsioPDS/node.h
@@ -2,19 +2,23 @@
 #include <memory>
 #include <string>
 #include <filesystem>
+#include <utility>
+#include <unordered_map>
 #include <boost/asio.hpp>
 #include <boost/uuid/uuid.hpp>
-#include <boost/uuid/random_generator.hpp>
-#include <boost/uuid/uuid_io.hpp>  
-#include <AsioPDS/discoveryListener.h>
+#include "AsioPDS/discoveryListener.h"
+#include "peer.h"
+
+
+struct DiscoveryPacket; 
 
 class Node
 {
 private:
     boost::asio::io_context& io;
     boost::uuids::uuid uuid_;
-    static constexpr uint8_t version = 1;
     boost::uuids::uuid loadOrCreateNodeUuid();
+    void handleDiscoveryPacket(DiscoveryPacket const& packet, boost::asio::ip::udp::endpoint const& sender);
     static std::filesystem::path userConfigurationDirectory();
     static std::filesystem::path uuidFilePath();
     std::shared_ptr<DiscoveryListener> listener_; 
@@ -22,14 +26,5 @@ private:
 
 
 public:
-    Node(boost::asio::io_context& ioCtx)
-    :  io(ioCtx) 
-    {
-        uuid_ = loadOrCreateNodeUuid();
-        std::cout << "UUID: " << uuid_ << '\n';
-        std::cout << "UUID File PAth: " << uuidFilePath() << '\n';
-
-        listener_ = std::make_shared<DiscoveryListener>(io, version);
-        listener_->start();
-    }
+    Node(boost::asio::io_context& ioCtx);
 };
