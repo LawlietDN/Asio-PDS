3125d5e8ba638d15109e067b2b36ce09f0411db3
LawlietDN <ufcgamer82@gmail.com>
2025-08-08 20:16:41 +0300

    proto.h: Expanded protocol constants.

--- Include/AsioPDS/proto.h
+++ Include/AsioPDS/proto.h
@@ -3,14 +3,18 @@
 
 namespace proto
 {
-    inline constexpr uint32_t Magic   = 0xDEADBEEF;
-    inline constexpr uint8_t  Version = 0x01;
-    inline constexpr uint16_t DiscoveryPort = 54321;
+    constexpr uint32_t Magic           = 0xDEADBEEF; //This is only a temporary solution.
+    constexpr uint8_t  Version         = 0x01;
+    constexpr uint16_t DiscoveryPort   = 54321;   
+    // constexpr uint8_t  MTHELLO        = 0x10;  
+    // constexpr uint8_t  MTSUBSCRIBE    = 0x01;   
+    // constexpr uint8_t  MTPUBLISH      = 0x03;  
 
-    enum class MsgType : std::uint8_t
-    {
-        Subscribe          = 0x01,
-        PropagateSubscribe = 0x02,
-        Publish            = 0x03
-    };
+
+    // enum class MsgType : std::uint8_t
+    // {
+    //     Subscribe          = 0x01,
+    //     PropagateSubscribe = 0x02,
+    //     Publish            = 0x03
+    // };
 } 
