0aa79f9455b508d4a4e6a57dec2748bf4632111f
LawlietDN <ufcgamer82@gmail.com>
2025-08-08 20:17:02 +0300

    main.cpp: Updated entry point for multi-node testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <utility>
 #include <boost/asio.hpp>
-#include <AsioPDS/node.h>
-int main()
+#include "AsioPDS/node.h"
+#include "AsioPDS/logging.h"
+
+int main(int argc,char* argv[])
 {
+    int verbosity = std::count_if(argv,argv+argc,[](auto* s){ return !std::strcmp(s,"-v");});
+    pdslog::init(verbosity==0?pdslog::info:verbosity==1?pdslog::debug:pdslog::trace);
     try
     {
         boost::asio::io_context io;
-        Node node(io);
+        Node a(io, 9102, ".pds_A");
+        Node b(io, 9202, ".pds_B");
         io.run();            
     }
     catch(std::exception const& e)
