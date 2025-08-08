aa739a8a14ff27c8c0446600cac265a186c967ce
LawlietDN <ufcgamer82@gmail.com>
2025-08-08 20:16:23 +0300

    peer.h: Defined peer connection state model.

--- Include/AsioPDS/peer.h
+++ Include/AsioPDS/peer.h
@@ -1,14 +1,15 @@
+#include <memory>
 #include <boost/uuid/uuid.hpp>
 #include <boost/asio.hpp>
 
-using TcpPort = decltype(
-    std::declval<boost::asio::ip::tcp::endpoint>().port());
-
+enum class PeerState { Discovered, Connecting, Connected };
 struct Peer
 {
-    boost::uuids::uuid PeerUUID;
-    TcpPort PeerTcpPort;
+    boost::uuids::uuid id;
     boost::asio::ip::tcp::endpoint PeerEndpoint;
+    std::shared_ptr<boost::asio::ip::tcp::socket> controlSocket;
+    PeerState state = PeerState::Discovered;
     std::chrono::steady_clock::time_point lastSeen;
+    bool helloSent{false};  
 
 };
