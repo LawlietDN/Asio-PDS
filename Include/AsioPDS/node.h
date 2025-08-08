4d520800ca8f4ffeca0d00ea9bbf0058c6a7e977
LawlietDN <ufcgamer82@gmail.com>
2025-08-08 20:02:01 +0300

    node.h/cpp Implemented peer state machine and TCP handshake.

--- Include/AsioPDS/node.h
+++ Include/AsioPDS/node.h
@@ -4,10 +4,14 @@
 #include <filesystem>
 #include <utility>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
 #include <boost/asio.hpp>
 #include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_hash.hpp>
 #include "AsioPDS/discoveryListener.h"
-#include "peer.h"
+#include <AsioPDS/discoveryBroadcaster.h>
+#include "AsioPDS/peer.h"
 
 
 struct DiscoveryPacket; 
@@ -17,14 +21,27 @@ class Node
 private:
     boost::asio::io_context& io;
     boost::uuids::uuid uuid_;
-    boost::uuids::uuid loadOrCreateNodeUuid();
+
+    boost::asio::ip::tcp::acceptor acceptor;
+    std::shared_ptr<DiscoveryListener> listener;
+    std::shared_ptr<DiscoveryBroadcaster> broadcaster;
+
+    std::unordered_map<boost::uuids::uuid, Peer, boost::hash<boost::uuids::uuid>> peers;
+    std::queue<boost::uuids::uuid> dialQueue;
+    std::unordered_set<boost::uuids::uuid, boost::hash<boost::uuids::uuid>> busy;
+
     void handleDiscoveryPacket(DiscoveryPacket const& packet, boost::asio::ip::udp::endpoint const& sender);
-    static std::filesystem::path userConfigurationDirectory();
-    static std::filesystem::path uuidFilePath();
-    std::shared_ptr<DiscoveryListener> listener_; 
+    void pumpDial();
+    void sendHello(std::shared_ptr<boost::asio::ip::tcp::socket> const& socket);
+    void readHello(std::shared_ptr<boost::asio::ip::tcp::socket> const& socket);
+    void startAccept();
+
+    static boost::uuids::uuid loadOrCreateNodeUuid(std::string const& tag);
+    static std::filesystem::path userConfigurationDirectory(std::string const& tag);
+    static std::filesystem::path uuidFilePath(std::string const& tag);
 
 
 
 public:
-    Node(boost::asio::io_context& ioCtx);
+    explicit Node(boost::asio::io_context& ioCtx, uint16_t tcpPort, std::string const& dataTag);
 };
--- src/Node/node.cpp
+++ src/Node/node.cpp
@@ -4,86 +4,203 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/random_generator.hpp>
 #include <boost/uuid/uuid_io.hpp>  
+#include <boost/uuid/uuid_hash.hpp>
 #include "AsioPDS/node.h"
 #include "AsioPDS/discoveryPacket.h" 
+#include "AsioPDS/logging.h"
 
-Node::Node(boost::asio::io_context& io_)
-    : io(io_) 
+Node::Node(boost::asio::io_context& io_, uint16_t tcpPort, std::string const& dataTag)
+    :   io(io_),
+        uuid_(loadOrCreateNodeUuid(dataTag)),
+        acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), tcpPort))
+
 {
-    uuid_ = loadOrCreateNodeUuid();
-    listener_ = std::make_shared<DiscoveryListener>(io, 
+    LOG(info) << "Node: " << uuid_ << " Listening on TCP: " << tcpPort << '\n';
+    listener = std::make_shared<DiscoveryListener>(io, 
         [this](DiscoveryPacket const& packet, boost::asio::ip::udp::endpoint const& sender) 
         {
             this->handleDiscoveryPacket(packet, sender);
         }
     );
+    listener->start(); 
+    broadcaster = std::make_shared<DiscoveryBroadcaster>(io_, uuid_, tcpPort);
+    broadcaster->start();
+    peers.reserve(256);
+    startAccept();
 
-    listener_->start();
 }
 
 void Node::handleDiscoveryPacket(DiscoveryPacket const& packet, boost::asio::ip::udp::endpoint const& sender)
 {
 
-    boost::uuids::uuid peerUuid;
-    std::memcpy(peerUuid.data, packet.uuid, 16);
-    if(peerUuid == uuid_)
+    boost::uuids::uuid peerUUID_;
+    std::memcpy(peerUUID_.data, packet.uuid, 16);
+    if(peerUUID_ == uuid_)
     {
-        std::cout << "Self broadcast\n";
+        LOG(debug) << "Ignored a self broadcast packet\n";
         return;
     }
-    if(!peers.contains(peerUuid))
+    
+    auto [position, isNew] = peers.try_emplace(
+        peerUUID_,
+        Peer
+        {
+            .id = peerUUID_,
+            .PeerEndpoint =  {sender.address(), ntohs(packet.tcpPort)},
+            .state = PeerState::Discovered,
+            .lastSeen = std::chrono::steady_clock::now()
+        }
+        );
+
+    auto& peer = position->second;
+    peer.lastSeen = std::chrono::steady_clock::now();
+
+    if (isNew)
     {
-        std::cout << "New Peer discovered\n";
+        LOG(info)<< "New peer discovered, peer list has been updated.\n";
     }
-    std::cout << "Peer is already in the peer list\n";
-
-        std::cout << "Magic: 0x" 
-              << std::hex << std::setw(8) << std::setfill('0')
-              << ntohl(packet.magic) << std::dec << "\n"
-              
-              << "Version: " << static_cast<int>(packet.version) << "\n"
-              
-              << "UUID: ";
+    else
+    {
+        LOG(info) << "Seen an already existing peer again, timestamp has been updated.\n";
+    }
     
-    boost::uuids::uuid uuid;
-    std::memcpy(uuid.data, packet.uuid, 16);
-    std::cout << uuid << "\n"
-              
-              << "TCP Endpoint: " << sender.address() << ':' << ntohs(packet.tcpPort) << "\n";
+    if(peer.state==PeerState::Discovered && busy.insert(peerUUID_).second)
+    {
+        dialQueue.push(peerUUID_);
+        pumpDial();
+    }
+
+}
+
+void Node::pumpDial()
+{
+    if(dialQueue.empty()) return;
+    auto id = dialQueue.front(); dialQueue.pop();
+    auto& peer = peers.at(id);
+
+    peer.state = PeerState::Connecting;
+    auto socket = std::make_shared<boost::asio::ip::tcp::socket>(io);
+    socket->async_connect(peer.PeerEndpoint, [this,socket,id](auto e)
+    {
+        busy.erase(id);
+        if(e)
+        {
+            peers.at(id).state = PeerState::Discovered;
+            return pumpDial();
+        }
+        sendHello(socket);
+        pumpDial();
+    });
+}
+
+void Node::sendHello(std::shared_ptr<boost::asio::ip::tcp::socket> const& socket)
+{
+    std::array<uint8_t,23> buffer{};
+    uint32_t* pm = reinterpret_cast<uint32_t*>(buffer.data());
+    *pm = htonl(proto::Magic);
+    buffer[4] = proto::Version;
+    std::memcpy(&buffer[5], uuid_.data,16);
+    boost::asio::async_write(*socket, boost::asio::buffer(buffer),
+        [this,socket](auto e, size_t){
+            if(e) return;
+            readHello(socket);
+        });
+}
+
+void Node::readHello(const std::shared_ptr<boost::asio::ip::tcp::socket>& socket)
+{
+    auto buffer = std::make_shared<std::array<uint8_t,23>>();
+    boost::asio::async_read(*socket, boost::asio::buffer(*buffer),
+        [this, socket, buffer](auto e, size_t)
+        {
+            if (e) return;
+            if (ntohl(*reinterpret_cast<uint32_t*>(buffer->data())) != proto::Magic) return;
+            if (buffer->at(4) != proto::Version)                                      return;
+
+            boost::uuids::uuid pid;
+            std::memcpy(pid.data, &(*buffer)[5], 16);
+            auto existing = peers.find(pid);
+            if (existing != peers.end() && existing->second.state == PeerState::Connected)
+            {
+            socket->close();
+            return;
+            }
+
+            auto [it, _] = peers.try_emplace(
+                pid,
+                Peer
+                {
+                      pid,
+                      socket->remote_endpoint(),    
+                      socket,
+                      PeerState::Connected,
+                      std::chrono::steady_clock::now() });
+
+            auto& peer = it->second;
+            peer.controlSocket = socket;
+            peer.state         = PeerState::Connected;
+            peer.lastSeen      = std::chrono::steady_clock::now();
+
+            if (!peer.helloSent)
+            {
+                sendHello(socket);      
+                peer.helloSent = true;
+            }
+            LOG(info) << "Peer " << pid << " connected";
+        });
+}
+
+void Node::startAccept()
+{
+    auto socket = std::make_shared<boost::asio::ip::tcp::socket>(io);
+    acceptor.async_accept(*socket, [this,socket](auto e)
+    {
+        if(!e) readHello(socket);
+        startAccept();
+    });
 }
 
 
-std::filesystem::path Node::userConfigurationDirectory()
+std::filesystem::path Node::userConfigurationDirectory(std::string const& tag)
 {
-    #ifdef _WIN32
-    const char* base = std::getenv("APPDATA");           
-    if (!base) throw std::runtime_error("APPDATA not set");
-    return std::filesystem::path(base) / "pds";                       
+#ifdef _WIN32
+    const char* base = std::getenv("APPDATA");
 #else
-    const char* base = std::getenv("HOME");              
-    if (!base) throw std::runtime_error("HOME not set");
-    return std::filesystem::path(base) / ".pds";                     
+    const char* base = std::getenv("HOME");
 #endif
-
+    if(!base) throw std::runtime_error("HOME/APPDATA not set");
+    return std::filesystem::path(base) / tag; 
 }
-std::filesystem::path Node::uuidFilePath() { return userConfigurationDirectory() / "node.uuid"; }
 
-boost::uuids::uuid Node::loadOrCreateNodeUuid()
+
+
+std::filesystem::path Node::uuidFilePath(std::string const& tag) { return userConfigurationDirectory(tag) / "node.uuid"; }
+
+boost::uuids::uuid Node::loadOrCreateNodeUuid(std::string const& tag)
 {
-    std::filesystem::path directory  = userConfigurationDirectory();
-    std::filesystem::path file = uuidFilePath();
+    std::filesystem::path directory  = userConfigurationDirectory(tag);
+    std::filesystem::path file = uuidFilePath(tag);
     
-    if (!std::filesystem::exists(directory)) std::filesystem::create_directories(directory);
+    if (!std::filesystem::exists(directory))
+    {
+        LOG(info) << "User configuration directory does not exist.\n";
+        LOG(info) <<  "Creating directory: " << directory << '\n';
+        std::filesystem::create_directories(directory);
+    }
 
     if (std::filesystem::exists(file))
     {
+        LOG(info) << "UUID file exists at: " << file << '\n';
+        LOG(info) << "Loading exisiting UUID..\n";
         std::ifstream in(file);
         boost::uuids::uuid id;
         in >> id;                         
-        if (in) return id;
+        if (in) LOG(info)  << "Loading successful.\n";
+        return id;
     }
-
+    LOG(info) << "No existing UUID file found. Generating a new UUID...\n";
     boost::uuids::uuid id = boost::uuids::random_generator()();
-    std::ofstream(file) << id;            
+    std::ofstream(file) << id;
+    LOG(info) << "UUID generation has been successful. Stored in: " << file << '\n';
     return id;
 }
